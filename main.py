20594841c52aeaa1c70c2ee5fdf6b0b28ca25161
Tolga <kurosuisho@proton.me>
2024-12-03 15:52:32 +0100

    change data file location to '_data', refactor imports and file paths

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from storage_json import StorageJson
+from _data.storage_json import StorageJson
 from movie_app import MovieApp
 
 def main():
     """Main function of the program."""
     # Create a StorageJson object with the path to the JSON file
-    storage = StorageJson("movies_list.json")
+    storage = StorageJson("_data/movies_list.json")
 
     # Create a MovieApp object with the StorageJson object
     app = MovieApp(storage)
